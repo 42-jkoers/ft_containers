c8867b6d000091ecc0a286ba03817034be09fcb4
SirMorfield <jkoers@student.codam.nl>
2021-11-18 16:26:09 +0100

    first few functions

---
+++ include/Vector.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+#include <limits>
+#include <memory>
+#define STARTSIZE 1
+
+namespace ft {
+
+template <typename T>
+class vector {
+  public:
+	typedef typename std::size_t size_type;
+
+	vector<T>() : _content(NULL), _capacity(0), _size(0){};
+	~vector<T>(){};
+
+	void push_back(const T& x) {
+		if (!_capacity)
+			_grow(STARTSIZE);
+		else if (_size == _capacity)
+			_grow(_capacity * 2);
+		_content[_size++] = x;
+	}
+
+	void reserve(size_type new_cap) {
+		// TODO: allocator exception
+		// TODO: If an exception is thrown, this function has no effect
+		if (new_cap > max_size())
+			throw std::length_error("allocator<T>::allocate(size_t n) 'n' exceeds maximum supported size");
+		if (new_cap <= _capacity)
+			return;
+		_grow(new_cap);
+	}
+	size_type size() const { return _size; };
+	size_type capacity() const { return _capacity; }
+	size_type max_size() const { // TODO: ??
+		const size_t elemSize = sizeof(T) == 1 ? 2 : sizeof(T);
+		return SIZE_T_MAX / elemSize;
+	}
+
+  protected:
+  private:
+	void _grow(size_t newCapacity) {
+		T* old = _content;
+		_content = _allocator.allocate(newCapacity);
+		_capacity = newCapacity;
+		if (!old)
+			return;
+		std::memcpy(_content, old, _size * sizeof(T));
+		_allocator.deallocate(old, _size);
+	}
+	T*				  _content;
+	size_t			  _capacity;
+	size_t			  _size;
+	std::allocator<T> _allocator;
+};
+
+} // namespace ft
