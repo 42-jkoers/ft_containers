3e982b3dafa442b4bce51d8913fca2ca609bf51b
SirMorfield <jkoers@student.codam.nl>
2021-12-17 15:54:10 +0100

    add ft::pair

--- include/iterator_traits.hpp
+++ include/iterator_traits.hpp
@@ -64,4 +64,47 @@ class iterator_traits<const T*> {
 	typedef ft::random_access_iterator_tag iterator_category;
 };
 
+template <class T1, class T2>
+struct pair {
+	typedef T1	first_type;
+	typedef T2	second_type;
+
+	first_type	first;
+	second_type second;
+
+	pair() : first(), second() {}
+
+	template <class U1, class U2>
+	pair(const pair<U1, U2>& p) : first(p.first), second(p.second) {}
+
+	pair(const T1& x, const T2& y) : first(x), second(y) {}
+
+	pair& operator=(const pair& x) {
+		first = x.first;
+		second = x.second;
+		return *this;
+	}
+};
+
+template <class T1, class T2>
+bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) { return lhs.first == rhs.first && lhs.second == rhs.second; }
+
+template <class T1, class T2>
+bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) { return lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second); }
+
+template <class T1, class T2>
+bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) { return !(lhs == rhs); }
+
+template <class T1, class T2>
+bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) { return (rhs < lhs); }
+
+template <class T1, class T2>
+bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) { return !(rhs < lhs); }
+
+template <class T1, class T2>
+bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) { return !(lhs < rhs); }
+
+template <class T1, class T2>
+pair<T1, T2> make_pair(T1 x, T2 y) { return pair<T1, T2>(x, y); }
+
 } // namespace ft
